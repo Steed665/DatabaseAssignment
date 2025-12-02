f55e7f5a0de0f52398773801715fd2db02f2a960
767490 <A.Steed-2022@hull.ac.uk>
2025-12-02 08:13:36 +0000

    step2

--- whois/whois/obj/Debug/net8.0/whois.AssemblyInfo.cs
+++ whois/whois/obj/Debug/net8.0/whois.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("whois")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa3d6cc07c20411ef662bc5c49fa9e0bbebc793")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad13c6e97b1dd2414a231892196bd3357b387d69")]
 [assembly: System.Reflection.AssemblyProductAttribute("whois")]
 [assembly: System.Reflection.AssemblyTitleAttribute("whois")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
